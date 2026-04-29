57b0bccf893911dc3aa17eeaa98cbfba8daf2969
ravisarvaiya-2007 <240130510059.cs@gujaratvidyapith.org>
2026-04-29 21:29:32 +0530

    Update children.php

--- children.php
+++ children.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VBG7ED5SH7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VBG7ED5SH7');
+</script>
+  
 <title>Children Photography</title>
 <link rel="stylesheet" href="style.css">
 </head>
